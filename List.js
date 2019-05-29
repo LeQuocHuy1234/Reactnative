4d2748fd707daa2bd8210e32cd547ed3ad1f4f42
Lê Quốc Huy <lequochuysv@gmail.com>
2019-05-29 17:22:28 +0700

    ListView ko nen dung

---
+++ List.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+
+class List extends Component{
+    state = {
+        names: [
+            {
+                id: 0,
+                name: 'Ben'
+            },
+            {
+                id: 1,
+                name: 'Susan'
+            }
+        ]
+    }
+    alertItemName = (item) => {
+        alert(item.name);
+    }
+    render() {
+        return (
+            <View>
+                {
+                    this.state.names.map((item, index) => (
+                        <TouchableOpacity
+                            key = {item.id}
+                            style = {styles.container}
+                            onPress = { () => this.alertItemName(item) }
+                        >
+                            <Text>{item.name}</Text>
+                        </TouchableOpacity>
+                    ))
+                }
+            </View>
+        )
+    }
+}
+
+export default List
+
+const styles = StyleSheet.create ({
+    container: {
+        padding: 10,
+        marginTop: 3,
+        backgroundColor: '#d9f9b1',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#4f603c'
+    }
+})
