d22144d003b7ae44ae8bf893e92f5a7194c7d2d4
Lê Quốc Huy <lequochuysv@gmail.com>
2019-05-30 11:11:24 +0700

    modal

--- App.js
+++ App.js
@@ -7,43 +7,47 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+    state = {
+        modalVisible: false,
+    };
 
-type Props = {};
-export default class App extends Component<Props> {
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <View style={{marginTop: 22}}>
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                }}>
+                <View style={{marginTop: 22}}>
+                    <View>
+                        <Text>Hello World!</Text>
+
+                        <TouchableHighlight
+                            onPress={() => {
+                                this.setModalVisible(!this.state.modalVisible);
+                            }}>
+                            <Text>Hide Modal</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            </Modal>
+
+            <TouchableHighlight
+                onPress={() => {
+                    this.setModalVisible(true);
+                }}>
+                <Text>Show Modal</Text>
+            </TouchableHighlight>
+        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
