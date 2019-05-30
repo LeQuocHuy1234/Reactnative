7245a871c151bf1f5fcd648d76246b5e1c836fcb
Lê Quốc Huy <lequochuysv@gmail.com>
2019-05-30 11:06:56 +0700

    FlatList

--- App.js
+++ App.js
@@ -5,30 +5,45 @@
  * @format
  * @flow
  */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import flatListData from './data/flatListData';
+import { Dimensions } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class FlatListItem extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    let width = Dimensions.get('window').width;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <View style = {{
+          flex: 1,
+          width: width,
+          padding: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: this.props.index % 2 == 0 ? 'red' :'green',
+          borderWidth: 1,
+        }}>
+          <Text>{ this.props.item.name }</Text>
+        </View>
+    )
+  }
+}
+export default class App extends Component {
+  render() {
+    console.log('hoang');
+    return (
+        <View style={styles.container}>
+          <FlatList
+              data = { flatListData }
+              renderItem = {({item, index}) =>
+                <FlatListItem item = { item } />
+              }
+          />
+        </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ data/flatListData.js
@@ -0,0 +1,20 @@
+const flatListData = [
+    {
+        "key": "1",
+        "name": "Le Quoc Huy"
+    },
+    {
+        "key": "2",
+        "name": "Hoang Van Thang"
+    },
+    {
+        "key": "3",
+        "name": "Le Van Doanh"
+    },
+    {
+        "key": "4",
+        "name": "Nguyen Van Teo"
+    }
+];
+
+export default flatListData;
